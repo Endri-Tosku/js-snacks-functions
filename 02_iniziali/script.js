299b18e4312d3e9309336221df7e31d9451ca967
Endri-Tosku <endritosku001@gmail.com>
2025-12-09 15:20:00 +0100

    feat: svolto l'es. delle iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function getInitials(iniziale) {
+    const result = [];
 
+    for (let i = 0; i < iniziale.length; i++) {
+        const initial = iniziale[i][0]; // mi prende solo la prima lettera del nome
+        result.push(initial);
+    }
+
+    return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(getInitials(names));
 
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
+
